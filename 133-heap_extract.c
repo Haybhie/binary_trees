78cbe60e4b1e1f983274a868a4b123eeb59ec91c
Haybhie <Haybhiee@gmail.com>
2024-03-01 03:09:12 +0000

    more binary forest port

---
+++ 133-heap_extract.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+/**
+ * tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height
+ *
+ * Return: Height or 0 if tree is NULL
+ */
+size_t tree_height(const heap_t *tree)
+{return}
